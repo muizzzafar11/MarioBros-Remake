b36212d32cd94d43eef290e727ac6dda2bdd5d9d
Albert Lee <1leealb@hdsb.ca>
2019-12-17 14:39:04 -0500

    Updated Speed and Image Size

--- Barrel.java
+++ Barrel.java
@@ -7,21 +7,27 @@ import greenfoot.*;
  * @version (a version number or a date)
  */
 public class Barrel extends Actor
-{
+{   //Size of the barrel
+    public Barrel (){
+        GreenfootImage myImage = getImage();
+        myImage.scale(50,50);
+    }
     public void act() 
     {
         if(isAtEdge())
         {
             getWorld().removeObject(this);
         } else{
-          setLocation(getX(), getY() + 3);
+          //Speed of the barrel?
+          setLocation(getX(), getY() + 4);
           while(isTouching(Floor2.class))
           {
+            //Speed of the barrel on the floor2?  
             setLocation(getX() - 3, getY() - 3);
             turn(-8);
           }
           while(isTouching(Floor.class))
-          {
+          { //Speed of the barrel on the floor?
             setLocation(getX() + 3, getY() - 3);
             turn(8);
           }
