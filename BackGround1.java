fed40bb813e33f93028606ccdadfdda6668829df
TahaM <34377163+TahaM1@users.noreply.github.com>
2019-12-31 03:01:24 -0500

    The display size of the game now scales according to the size of your monitor

--- BackGround1.java
+++ BackGround1.java
@@ -6,16 +6,20 @@ import greenfoot.*;
  * @author (your name) 
  * @version (a version number or a date)
  */
+import java.awt.Toolkit;
+import java.awt.Dimension;
 public class BackGround1 extends World
 {
-
+    public static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    public static int width = (int)screenSize.getWidth();
+    public static int height = (int)screenSize.getHeight();
     /**
      * Constructor for objects of class BackGround1.
      * 
      */
     public BackGround1()
     {    
-        super(1500, 1000, 1); 
+        super((int)(screenSize.getWidth()/2.5), (int)(screenSize.getHeight()/1.25), 1);         
         addObject(new DK(), 200, 150);
         addObject(new Floor(), 125, 219);
         addObject(new Floor(), 375, 219);
@@ -35,6 +39,8 @@ public class BackGround1 extends World
         addObject(new Floor2(), 875, 1000);
         addObject(new Floor2(), 1125, 1000);
         addObject(new Floor2(), 1375, 1000);
-        addObject(new Mario(), 125, 940);
+        addObject(new Run("mario/run_right/mario_",0,3,"right", 1), 500, 400);
     }
+    
+   
 }
