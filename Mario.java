9da38d70eb9d8b7479afe5bf5623a578dccd728c
TahaM <34377163+TahaM1@users.noreply.github.com>
2019-12-31 03:40:16 -0500

    mario class now controls which mario subclasses are being displayed

--- Mario.java
+++ Mario.java
@@ -1,84 +1,70 @@
+
+
 import greenfoot.*;
 public class Mario extends Actor
 {
     int speed;
-    String Marioimage = "mariopixelCopy.png";
+    //String Marioimage = "mario/running_right/mario_00.png";
     long lastTime;
     int Lives = 3;
-    public void act() 
-    {
-        speed = speed + 1;
-        setLocation( getX(), getY() + speed);
-        getWorld().showText("Lives : "+ Lives +"",1450, 50);
-        if(isTouching(Barrel.class))
-        {
-            removeTouching(Barrel.class);
-            Lives = Lives - 1;
+    int px, py, vx, vy;
+    int height, width;
+    String key = "";
+    String[] possibleKeys = {"left", "right", "up"};
+    
+    
+    public Mario(String key){
+       this.key = key;
+    }
+  
+    protected void addedToWorld(World world){
+       height = getWorld().getHeight();
+       width = getWorld().getWidth();
+    }
+    
+    public Actor floorHitDetection(){
+        Actor object  = getOneObjectAtOffset(getImage().getWidth()/2, getImage().getHeight()/2, Floor.class);
+        /*
+        if(object != null){
+            return true;
         }
-        if(Lives == 0)
-        {
-            getWorld().showText("GAME OVER", 750, 600);
-            Greenfoot.stop();
-        }
-        if(speed > 0)
-        {
-            while(isTouching(Floor.class))
-            {
-                speed = 0;
-                setLocation(getX(), getY() - 1);
-                if(Greenfoot.isKeyDown("up"))
-                {
-                    speed = - 27;
+        */
+        return object;
+    }
+    
+    public boolean otherKeyPressed(int px, int py){
+       
+            int index = -1;
+            for(int i = 0; i < possibleKeys.length; i++){
+                if(Greenfoot.isKeyDown(possibleKeys[i]) && possibleKeys[i] != key){
+                    if(!(Greenfoot.isKeyDown("left") && Greenfoot.isKeyDown("right"))){
+                        index = i;
+                        break;
+                    }
                 }
             }
-        }
-        if(speed <= 0)
-        {
-            while(isTouching(Floor.class))
-            {
-                speed = 0;
-                setLocation(getX(), getY() + 1);
-            }
-        }    
-        if(Greenfoot.isKeyDown("left"))
-        {
-            move(-5);
-            /*if(System.currentTimeMillis() - lastTime > 500 && Marioimage.equals("mariopixelCopy.png"))
-            {
-                Marioimage = "marioleft.png";
-                setImage("marioleft.png");
-                lastTime = System.currentTimeMillis();
-            } else {
-                if(System.currentTimeMillis() - lastTime > 500)
-                {
-                    Marioimage = "mariopixelCopy.png";
-                    setImage("mariopixelCopy.png");
-                    lastTime = System.currentTimeMillis();
-                }
-            } */
+            switch(index){
             
-           
-            setImage("mariopixelCopy.png");
-            while(isTouching(Floor.class))
-            {
-               move(1);
-            } 
-        } else {
-            if(Greenfoot.isKeyDown("right"))
-            {
-               move(5);
-               setImage("mariopixel.png");
-                while(isTouching(Floor.class))
-                {
-                  move(-1);
-               }
-            } else{
-                setImage("mario-big.png");
+                case 0:
+                    getWorld().addObject(new Run("mario/run_left/mario_",3, 6, "left", -1), px, py);
+                    break;
+                case 1:
+                    getWorld().addObject(new Run("mario/run_right/mario_", 0, 3, "right", 1), px, py);
+                    break;
+                case 2: 
+                
+                    break;
+                case 3: 
+                
+                    break;
+                
             }
-        }
-        if(Greenfoot.isKeyDown("down"))
-        {
-            speed = 50;
-        }
-    } 
+            if(index != -1){
+                return true;
+            }
+            
+        
+        return false;
+    }
+    
 }
