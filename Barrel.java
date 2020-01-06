2ab27419150a6afc92198d0e6457d3329790edf3
Albert Lee <1leealb@hdsb.ca>
2020-01-06 14:04:53 -0500

    BarrelAnimations
    Added Animations to the barrel

--- Barrel.java
+++ Barrel.java
@@ -7,31 +7,58 @@ import greenfoot.*;
  * @version (a version number or a date)
  */
 public class Barrel extends Actor
-{   //Size of the barrel
-    public Barrel (){
-        GreenfootImage myImage = getImage();
-        myImage.scale(50,50);
-    }
+{   
+    private int frames = 1;
+    private int animationCounter = 0;
+    private GreenfootImage Barrel1 = new GreenfootImage("BarrelAni1.png");
+    private GreenfootImage Barrel2 = new GreenfootImage("BarrelAni2.png");
+    private GreenfootImage Barrel3 = new GreenfootImage("BarrelAni3.png");
+    private GreenfootImage Barrel4 = new GreenfootImage("BarrelAni4.png");
+    
     public void act() 
-    {
+    {   animationCounter++;
+        
         if(isAtEdge())
         {
             getWorld().removeObject(this);
         } else{
-          //Speed of the barrel?
-          setLocation(getX(), getY() + 4);
+          if(animationCounter%4 == 0){
+            Animation();
+        }
+          setLocation(getX(), getY() + 3);
           while(isTouching(Floor2.class))
           {
-            //Speed of the barrel on the floor2?  
             setLocation(getX() - 3, getY() - 3);
-            turn(-8);
+            
           }
           while(isTouching(Floor.class))
-          { //Speed of the barrel on the floor?
+          {
             setLocation(getX() + 3, getY() - 3);
-            turn(8);
+            
           }
        }
     }
+    public void Animation(){
+        if(frames == 1){
+            setImage(Barrel1);
+        }
+        
+        if(frames == 2){
+            setImage(Barrel2);
+        }
+        
+        if(frames == 3){
+            setImage(Barrel3);
+        }
+        
+        if(frames == 4){
+            setImage(Barrel4);
+            frames = 1;
+            return;
+        }
+        
+        frames ++;
+    }
 }
+    
  
