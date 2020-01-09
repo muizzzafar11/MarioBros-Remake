6714d333a7a69a12787fc643b96a423a215234f4
muizzzafar11 <43075405+muizzzafar11@users.noreply.github.com>
2020-01-09 14:32:46 -0500

    fixed coins scoring

--- Coins.java
+++ Coins.java
@@ -1,19 +1,14 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Coins here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Coins extends Actor
-{
-    /**
-     * Act - do whatever the Coins wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+{   
+    private static int coins = 0;
     public void act() 
     {
-        // Add your action code here.
+        if(isTouching(Mario.class)) {
+            getWorld().removeObject(this);
+            coins++;
+            System.out.println(coins);
+        }
     }    
 }
