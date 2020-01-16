60d5cab56e6614c056b0cfe72ada594191489192
muizzzafar11 <43075405+muizzzafar11@users.noreply.github.com>
2020-01-16 12:59:48 -0500

    Fixed Errors

--- BackGround1.java
+++ BackGround1.java
@@ -25,12 +25,43 @@ public class BackGround1 extends World
         scrollActor = new Mario();
 
         // Adding actor
-        addObject(scrollActor, 20, (height-120));
-        addObject(new DK(), 4500 , (height-120));
-        for(int i = 0; i <= worldLength; i += 200)// This is for the basic floor 
-            addObject(new Floor2(), i, height-50);
+// <<<<<<< Updated upstream
+        // addObject(scrollActor, 20, (height-120));
+        // addObject(new DK(), 4500 , (height-120));
+        // for(int i = 0; i <= worldLength; i += 200)// This is for the basic floor 
+            // addObject(new Floor2(), i, height-50);
+        // for(int i = 0; i < worldLength; i += 1000)// This is for the secondary floor, change after += to change the distance. 
+            // addObject(new Floor(), i, height/2);  
+// =======
+        addObject(scrollActor, 20, (height-60));
+        addObject(new DK(), 4500 , (height-60));
+        addObject(new Goomba(), 450 , (height-54));
+        addObject(new Turtle(), 3250 , (height-54));
+
+        // for(int i = 0; i <= worldLength; i += 200)// This is for the basic floor 
+            // addObject(new Floor2(), i, height-20);
         for(int i = 0; i < worldLength; i += 1000)// This is for the secondary floor, change after += to change the distance. 
-            addObject(new Floor(), i, height/2);  
+            addObject(new Floor(), i, height-20);
+        
+        /*super(width, height, 1); 
+        int mod = 2;
+        addObject(new DK(), 100, (int)(height/9.75));
+
+        for(int i = 100; i < height; i += floorDist) {
+        if((mod % 2) == 0){
+        for(int j = 0; j < width-100; j += 250) {
+        addObject(new Floor(), j, i);
+        }   
+        }else{
+        for(int k = width; k > 100; k -= 250) {
+        addObject(new Floor2(), k, i);
+        }
+        }
+        mod += 1;
+        }
+        // there is no floor between the distance of 75 then make a new floor otherwise bring the other floor down
+        addObject(new Mario(), 125, (height-60));*/
+// >>>>>>> Stashed changes
     }
 
     public void act() {
