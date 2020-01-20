297fe81a76642a2d205e7a15a5ea59f14811f1b7
AlbertLee986 <58993445+AlbertLee986@users.noreply.github.com>
2020-01-20 00:07:08 -0500

    TitleScreen

---
+++ TitleWords.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleWords here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleWords extends Actor
+{
+    public TitleWords(){
+        GreenfootImage myImage = getImage();
+        myImage.scale(400,200);
+    }
+    public void act() 
+    {
+        
+    }    
+}
